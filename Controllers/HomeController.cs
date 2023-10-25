e7aa8a8faceda25493d2b2a13392d30a58963233
DavidProgramer404 <davidvaldesprogramador@gmail.com>
2023-10-25 20:09:57 -0300

    Dojo_Survey_Model

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using DojoSurveyModel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DojoSurveyModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("result")]
+        public IActionResult Result(Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(survey);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
